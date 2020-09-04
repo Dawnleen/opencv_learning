095303ad8d58d4ea3a7b771d3d979ba6d13d09c8
framist <xnhu_1@stu.xidian.edu.cn>
2020-09-04 20:54:36 +0800

    233

---
+++ 数模最后一次模拟题/csdn1.py
@@ -0,0 +1,11 @@
+# https://blog.csdn.net/weixin_41869763/article/details/104067851
+# coding:UTF-8
+# v1--读取与显示图像
+import cv2
+import numpy as np
+
+img_bgr = cv2.imread('DATA1.jpg')   #读取图像
+
+cv2.imshow('原始图像', img_bgr)            #显示图像
+cv2.waitKey(0)
+cv2.destroyAllWindows()
